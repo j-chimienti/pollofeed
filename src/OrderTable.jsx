8067aefd92c050ba69a331b12102fac29ff0d09e
joe <jchimien@gmail.com>
2019-02-13 19:04:29 -0500

    t

--- src/OrderTable.jsx
+++ src/OrderTable.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from "react-router-dom";
+import './OrderGraph.css'
 
 function OrderRow({id, paid_at, completed_at, status, msatoshi, video}) {
 
