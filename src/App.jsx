2c177dde48e8d642fe7f81b469d20eec7ac03e46
joe <jchimien@gmail.com>
2019-02-26 16:50:35 -0500

    t

--- src/App.jsx
+++ src/App.jsx
@@ -200,6 +200,22 @@ class App extends Component {
                 />
                 <BrowserRouter>
                     <Switch>
+                        <Route path={'/twitch'} render={props =>
+                            <div>
+                                <iframe src="https://player.twitch.tv/?channel=pollofeed" frameBorder="0"
+                                        allowFullScreen="true" scrolling="no" height="378" width="620">
+
+                                </iframe>
+                                <a href = "https://www.twitch.tv/pollofeed?tt_content=text_link&tt_medium=live_embed"
+                                   style = "padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;" > Watch
+                                    live
+                                    video
+                                    from
+                                    pollofeed
+                                    on
+                                    www.twitch.tv </a>
+                                </div>
+                        } />
                         <Route path={'/about'} exact component={About}/>
                         <Route path={'/order/id/:id'}
                                render={props =>
