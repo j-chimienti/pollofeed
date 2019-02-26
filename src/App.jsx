a73a93aff7c43526078443ba201128fdfc3be3b2
joe <jchimien@gmail.com>
2019-02-26 16:59:52 -0500

    t

--- src/App.jsx
+++ src/App.jsx
@@ -206,14 +206,6 @@ class App extends Component {
                                         allowFullScreen="true" scrolling="no" height="378" width="620">
 
                                 </iframe>
-                                <a href = "https://www.twitch.tv/pollofeed?tt_content=text_link&tt_medium=live_embed"
-                                   style = "padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;" > Watch
-                                    live
-                                    video
-                                    from
-                                    pollofeed
-                                    on
-                                    www.twitch.tv </a>
                                 </div>
                         } />
                         <Route path={'/about'} exact component={About}/>
