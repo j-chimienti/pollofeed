52b1c5204c2607ff8d55e7c1c475d41c5eb8f8ce
joe <jchimien@gmail.com>
2019-02-27 20:59:50 -0500

    t

--- src/Home.jsx
+++ src/Home.jsx
@@ -131,7 +131,7 @@ export class Home extends Component {
 
                         <div className={'col mx-auto'} style={{maxWidth: '680px'}}>
                             <div className={'embed-responsive embed-responsive-4by3'}>
-                                <iframe src={'https://a178f033.ngrok.io/'} width={'640'} height={'480'}>
+                                <iframe src={'https://pollofeed.ngrok.io'} width={'640'} height={'480'}>
                                 </iframe>
                             </div>
                         </div>
