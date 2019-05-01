65ea4e96474b1b895276603e2f709a765c6937b1
joe <jchimien@gmail.com>
2019-05-01 09:17:32 -0400

    t

--- bin/feed.js
+++ bin/feed.js
@@ -1,9 +1,9 @@
 
-function _feed() {
+async function _feed() {
 
     const futureDate = new Date(new Date().getTime() + (86400000 * 1000)).getTime()
 
-    return db.orders.findOneAndUpdate({id: "testing"}, {$set:
+    return await global.db.orders.findOneAndUpdate({id: "testing"}, {$set:
             {feed: true, acknowledged_at: null, msatoshi: 0, paid_at: 0, pay_index: 99999999}
             }, {upsert: true, returnNewDocument: true, returnOriginal: false})
 
@@ -11,14 +11,20 @@ function _feed() {
 }
 
 
+async function sleep(ms = 1000) {
 
+    return new Promise(resolve => {
 
- function main(times = 2) {
+        setTimeout(resolve, ms)
+    })
+}
+
+async function main(times = 2) {
 
         for (let i = 1; i <= times; i++) {
             const result = _feed();
-            print(JSON.stringify(result, null, 4))
-            sleep(1000)
+            console.log(JSON.stringify(result, null, 4))
+            await sleep(1000)
 
         }
 }
