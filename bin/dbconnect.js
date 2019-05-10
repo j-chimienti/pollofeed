0dd41332271463ea626cf733a6efe81abf967906
joe <jchimien@gmail.com>
2019-05-10 09:35:05 -0400

    t

--- bin/dbconnect.js
+++ bin/dbconnect.js
@@ -6,7 +6,7 @@ module.exports = async function() {
     const client = await mongoConnect()
     console.log('Connected successfully to server')
 
-    const dbName = process.env.POLLOFEED_DB_NAME || (console.error('no db'), process.exit(1))
+    const dbName = process.env.POLLOFEED_DB_NAME || (console.error('no POLLOFEED_DB_NAME env'), process.exit(1))
 
     global.db = client.db(dbName)
 
