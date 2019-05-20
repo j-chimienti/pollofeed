c17b9359f8dca294aa6961b156b5b12e8b25b94c
joe <jchimien@gmail.com>
2019-05-19 22:17:55 -0400

    Update report

--- src/bin/calcFeedTimes.js
+++ src/bin/calcFeedTimes.js
@@ -1,22 +1,13 @@
 function calcFeedTimes(hours = new Date().getHours(), todayFeedCount = 0, yesterdayFeedCount = 0) {
-    const thresholds = {
-        "9": 10,
-        "13": 10,
-        "18": 15
-    }
+    const [threshold1, threshold2, threshold3] = [10,10,15]
     let feedTimes = 0
-    if (hours >= 17) {
-        feedTimes = thresholds['18'] - todayFeedCount
-    } else if (hours >= 13) {
-        feedTimes = thresholds['13'] - todayFeedCount
-    } else if (hours >= 9) {
-        if (yesterdayFeedCount >= 35) {
-            feedTimes = 0
-        }
-        else feedTimes = thresholds['9'] - todayFeedCount
+    if (hours >= 17) feedTimes = threshold3 - todayFeedCount
+    else if (hours >= 13) feedTimes = threshold2 - todayFeedCount
+    else if (hours >= 8) {
+        if (yesterdayFeedCount >= 35) feedTimes = 0
+        else feedTimes = threshold1 - todayFeedCount
     }
-    feedTimes = Math.min(4, Math.max(0, feedTimes))
-    return feedTimes
+    return Math.min(4, Math.max(0, feedTimes))
 }
 
 module.exports = calcFeedTimes
--- src/bin/report.js
+++ src/bin/report.js
@@ -3,47 +3,36 @@ require('dotenv').config({path: path.join(__dirname, '..', "..", '.env.developme
 const orderDao = require('../lib/invoices/dao')
 const dbconnect = require('./dbconnect')
 const {send} = require('./email')
-const totals = require('./totals')
-const marked = require('marked')
+// const totals = require('./totals')
 async function main() {
 
     await dbconnect()
     const todayOrders = await orderDao.getOrdersByDate()
-    let t = getTotals(todayOrders);
-    const text = `Todays feedings: ${t.orders}, Satoshis: ${t.satsoshis.toLocaleString()}`
-    const {
-        oldestOrderDate,
-        newEstOrderDate,
-        days,
-        min, // max feeding day
-        max, // min feedin day
-        avgDay,
-        satsTotal,
-        btc,
-    } = await totals()
+    let todayTotals = getTotals(todayOrders);
+    const text = `Todays feedings: ${todayTotals.orders}, Satoshis: ${todayTotals.satsoshis.toLocaleString()}`
+    // const {
+    //     oldestOrderDate,
+    //     newEstOrderDate,
+    //     days,
+    //     min, // max feeding day
+    //     max, // min feedin day
+    //     avgDay,
+    //     satsTotal,
+    //     btc,
+    // } = await totals()
 
-    const rawText = `
-    ### Summary\n
-    Today = ${t.orders}, sats = ${t.satsoshis.toLocaleString()}\n
-    Total = ${btc} btc, sats = ${satsTotal}\n
-    Days = ${parseInt(days)}\n
-    Start = ${oldestOrderDate.toLocaleString()}\n
-    End = ${newEstOrderDate.toLocaleString()}\n
-    Min = ${min.date.toLocaleString()} ${min.fed}\n
-    Max = ${max.date.toLocaleString()} ${max.fed}\n
-    Avg = ${avgDay}
+    const html = `<h3>Summary</h3>
+    <p>Today = ${todayTotals.orders}, sats = ${todayTotals.satsoshis.toLocaleString()}</p>
     `
-    const html = marked(rawText)
 
     const mailOptions = {
         from: process.env.GMAIL_USER, // sender address
         to: process.env.GMAIL_USER, // list of receivers
         subject: text,
         html
-    };
-
+    }
     await send(mailOptions)
-    return t;
+    return todayTotals;
 }
 
 function getTotals(orders) {
