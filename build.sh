16a7bcee1e19f4a09c71a26d8c072e5d7e20cc6c
joe <jchimien@gmail.com>
2020-02-13 20:02:26 -0600

    minify

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 # Bubble up errors
 set -xeo pipefail
-rm -rf ./dist/*
-babel -d dist src/client.js
-browserify ./src/client.js -o dist/client.bundle.min.js
+rm -rf dist
+./node_modules/.bin/browserify ./src/client.js -o dist/client.bundle.js
+# minify es6
+./node_modules/.bin/terser -c -o dist/client.bundle.min.js dist/client.bundle.js
+rm dist/client.bundle.js
 #cp src/manifest.json dist
 cp src/*.css dist
 cp src/img/*.png dist
 cp src/img/*.jpg dist
-# cp src/pollofeed_banner.png dist
