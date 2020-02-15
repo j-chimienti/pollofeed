a4080e4acc9a35eb4c199f747491ab59db4b18f4
joe <jchimien@gmail.com>
2020-02-15 11:46:59 -0600

    remove service worker as does not work with iframe in page

--- src/loadServiceWorker.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("./serviceWorker.js")
-            .then(r => console.log("service worker registered"))
-            .catch(err => console.error("service worker not registered", err))
-    })
-}
