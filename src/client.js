c928fdf1e76f9f84445b66010873cc527052a9b2
joe <jchimien@gmail.com>
2020-02-15 10:09:54 -0600

    init service worker and pwa

--- src/client.js
+++ src/client.js
@@ -96,3 +96,11 @@ setInterval(_ =>
 $(document).on('hidden.bs.modal', '.modal', e => $(e.target).remove())
 
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+        .register("./serviceWorker.js")
+        .then(r => console.log("service worker registered"))
+        .catch(err => console.error("service worker not registered", err))
+  })
+}
